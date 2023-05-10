5991fef5f8d6889fe44619b860631174b3a805e9
方钰 <fangyuf@wicresoft.com>
2023-05-10 16:00:43 +0800

    初始化项目

--- vite.config.ts
+++ vite.config.ts
@@ -7,59 +7,64 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import Unocss from 'unocss/vite'
 import {
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives,
-  transformerVariantGroup,
+    presetAttributify,
+    presetIcons,
+    presetUno,
+    transformerDirectives,
+    transformerVariantGroup,
 } from 'unocss'
 
 const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${pathSrc}/`,
+    server: {
+        host: 'localhost',
+        port: 5000,
+        open: true
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/element/index.scss" as *;`,
-      },
+    resolve: {
+        alias: {
+            '~/': `${pathSrc}/`,
+        },
     },
-  },
-  plugins: [
-    vue(),
-    Components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "~/styles/element/index.scss" as *;`,
+            },
+        },
+    },
+    plugins: [
+        vue(),
+        Components({
+            // allow auto load markdown components under `./src/components/`
+            extensions: ['vue', 'md'],
+            // allow auto import and register components used in markdown
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            resolvers: [
+                ElementPlusResolver({
+                    importStyle: 'sass',
+                }),
+            ],
+            dts: 'src/components.d.ts',
         }),
-      ],
-      dts: 'src/components.d.ts',
-    }),
 
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    Unocss({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons({
-          scale: 1.2,
-          warn: true,
+        // https://github.com/antfu/unocss
+        // see unocss.config.ts for config
+        Unocss({
+            presets: [
+                presetUno(),
+                presetAttributify(),
+                presetIcons({
+                    scale: 1.2,
+                    warn: true,
+                }),
+            ],
+            transformers: [
+                transformerDirectives(),
+                transformerVariantGroup(),
+            ]
         }),
-      ],
-      transformers: [
-        transformerDirectives(),
-        transformerVariantGroup(),
-      ]
-    }),
-  ],
+    ],
 })
